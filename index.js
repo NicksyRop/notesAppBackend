7463d463d7288c36e609446b470cf7c4b1b9ea4a
NicksyRop <nicksonkipkorir25@gmail.com>
2022-01-30 21:22:50 +0300

    added routes to add new note with validation

--- index.js
+++ index.js
@@ -43,6 +43,41 @@ app.get("/api/notes/:id", (req, res) => {
   res.json(note);
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
